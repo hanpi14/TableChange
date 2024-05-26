fd33a9eff5e873bf8685789fceb8814628d27aae
fangxw <fangxw@qq.com>
2024-05-26 22:12:33 +0800

    mysql=> sparksql => 202405262212

--- src/main/resources/New_20240521/调整/17.去除部分退费导致的服务费重复的问题.sql
+++ src/main/resources/New_20240521/调整/17.去除部分退费导致的服务费重复的问题.sql
@@ -14,6 +14,23 @@ WHERE A.income_reward!=0 AND B.income_reward !=0
 
 -- sparkSql on iceberg
 
+MERGE INTO dw.dwd.dwd_order_emp_di AS A USING(
+      SELECT * FROM dw.dwd.dwd_order_emp_mi WHERE income_reward !=0 AND B.is_refund=0 AND B.is_extra=0 AND  B.is_sup=0 AND B.is_cancel=0
+) B
+ON (
+A.data_source=B.data_source
+AND A.order_no=B.order_no
+AND A.order_type=B.order_type
+AND A.fee_month=B.fee_month
+AND A.emp_id_no=B.emp_id_no
+AND A.is_cancel=1
+AND A.is_refund=0
+AND A.is_extra=0
+AND A.is_sup=0
+AND A.income_reward!=0
+)
+WHEN MATCHED  THEN UPDATE SET A.income_reward=0;
+
 
 
 -- sparkSql Local
